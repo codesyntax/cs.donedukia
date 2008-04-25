8e50b1182d166226a33f925d322308fa08d7b6ce
erral <mlarreategi@codesyntax.com>
2008-04-25 13:07:29 +0000

    initial import
    svn path=/cs.donedukia/trunk/; revision=10755

---
+++ cs/donedukia/config.py
@@ -0,0 +1,8 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'cs.donedukia'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+}
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='cs.donedukia',
+      version=version,
+      description="Plone version of CodeSyntax's DonEdukia product",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='http://code.codesyntax.com/private/cs.donedukia',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
