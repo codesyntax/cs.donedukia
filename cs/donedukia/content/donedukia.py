f22f2680c372ece3a75e244898a0e9b0edea879e
erral <mlarreategi@codesyntax.com>
2010-11-03 12:27:58 +0000

    ips
    svn path=/cs.donedukia/trunk/; revision=38340

--- cs/donedukia/content/donedukia.py
+++ cs/donedukia/content/donedukia.py
@@ -17,42 +17,14 @@ except ImportError:
     from zope.i18nmessageid import MessageFactory
     AT_ = MessageFactory('atcontenttypes')
     
-DonEdukiaSchema = folder.ATFolderSchema.copy() + atapi.Schema((
-
-    # -*- Your Archetypes field definitions here ... -*-
-    atapi.TextField('text',
-                    required=False,
-                    searchable=True,
-                    storage=atapi.AnnotationStorage(),
-                    validators=('isTidyHtmlWithCleanup',),
-                    default_content_type='text/html',
-                    allowable_content_types =('text/html',),
-                    default_output_type='text/x-html-safe',
-                    widget=atapi.RichWidget(label=AT_(u"label_body_text", default=u'Body Text'),
-                                            description= '',
-                                            rows=25,
-                                            allow_file_upload=False),
-                    ),
-    ))
-
-
-    DonEdukiaSchema = DonEdukiaSchema.copy() + atapi.Schema((
-    
-        atapi.BooleanField('tableContents',
-                           required = False,
-                           widget = atapi.BooleanWidget(
-                                      label= AT_(u'help_enable_table_of_contents', default=u'Table of contents'),
-                                      description = AT_(u'help_enable_table_of_contents_description', default=u'If selected, this will show a table of contents at the top of the page.')
-                                      ),
-                           ),
-
-    ))
+DonEdukiaSchema = document.ATDocumentSchema.copy() + folder.ATFolderSchema.copy()
 
 # Set storage on fields copied from ATFolderSchema, making sure
 # they work well with the python bridge properties.
 
 DonEdukiaSchema['title'].storage = atapi.AnnotationStorage()
 DonEdukiaSchema['description'].storage = atapi.AnnotationStorage()
+DonEdukiaSchema['text'].storage = atapi.AnnotationStorage()
 
 schemata.finalizeATCTSchema(DonEdukiaSchema, folderish=True, moveDiscussion=False)
 # finalizeATCTSchema hides relatedItems for folderish items
