e575b7303c1ee80e60f3a3987a3c6abfbb992546
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-11-09 12:30:12 +0100

    remove useles plone version configuration constants

--- cs/donedukia/config.py
+++ cs/donedukia/config.py
@@ -7,17 +7,3 @@ ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'DonEdukia': 'cs.donedukia: Add DonEdukia',
 }
-
-from Products.CMFPlone.utils import getFSVersionTuple
-
-version = getFSVersionTuple()
-IS_PLONE2 = False
-IS_PLONE3 = False
-IS_PLONE4 = False
-
-if version[0] == 2:
-    IS_PLONE2 = True
-elif version[0] == 3:
-    IS_PLONE3 = True
-elif version[0] == 4:
-    IS_PLONE4 = True
